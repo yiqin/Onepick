22f39dc64a8a056074620eb2bc34afaf6326932d
yiqin <yiqin.mems@gmail.com>
2014-04-21 17:20:21 -0400

    Initial Commit

---
+++ onepick/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  onepick
+//
+//  Created by yiqin on 4/21/14.
+//  Copyright (c) 2014 purdue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ onepick/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  onepick
+//
+//  Created by yiqin on 4/21/14.
+//  Copyright (c) 2014 purdue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
