fc84e1eee02b72432be03c5e31912a235b5d0da9
yiqin <yiqin.mems@gmail.com>
2014-05-07 11:31:26 -0400

    Add MoreViewController. Location indicator is saved in user default.

---
+++ onepick/MoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoreViewController.h
+//  onepick
+//
+//  Created by yiqin on 5/7/14.
+//  Copyright (c) 2014 purdue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreViewController : UIViewController
+
+@end
